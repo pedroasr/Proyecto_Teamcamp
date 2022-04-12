3a8d363f4b4b2c23cf0cc3333f6de8a4f5c644c9
Pedro Antonio <pedroantoniosr96@gmail.com>
2022-04-12 22:48:17 +0200

    movie-service.ts será el módulo encargado de llamar a la base de datos. Este módulo se importará posteriormente. No están completas las llamadas a la base de datos, solamente son ejemplos tomados de los ejercicios disponibles. Cuando la base de datos tenga forma, se podrán implementar.

--- backend/src/database/movie-service.ts
+++ backend/src/database/movie-service.ts
@@ -6,21 +6,30 @@ export type Movie = {
 };
 
 export type MovieServices = {
-    list(page: number, pageSize: number, search: string),
+    list(page: number, pageSize: number, search: string) : Promise<Movie[]>,
     getById(id: string),
     getByFilter(filter: string | string[])
-    getByOrder(order: 'alphadescent' | 'alphaascent' | 'new' | 'old')
 };
 
 export function buildMovieService(db: SQL_DB,
                                   movieService : MovieServices){
     return{
-        async list(page: number, pageSize: number, search: string){
+        async list(page: number, pageSize: number, search: string, order: 'alphadescent' | 'alphaascent' | 'new' | 'old') : Promise<Movie[]>{
+            return await db.runQuery(
+                `INSERT INTO messages(message, target, origin) VALUES('${message.message}', '${message.to}', '${message.from}')`
+            );
+        },
+    
+        async getById(id: string) : Promise<Movie>{
+            return await db.runQuery(
+                `INSERT INTO messages(message, target, origin) VALUES('${message.message}', '${message.to}', '${message.from}')`
+            );
+        },
 
+        async getByFilter(filter: string | string[], order: 'alphadescent' | 'alphaascent' | 'new' | 'old') : Promise<Movie[]>{
+            return await db.runQuery(
+                `INSERT INTO messages(message, target, origin) VALUES('${message.message}', '${message.to}', '${message.from}')`
+            );
         }
     }
-
-
-
-
 } 
